bd18a8aff6506bc6e2de4bab881dcc61cadc1b75
vasil <vasilgmarkov@gmail.com>
2016-03-29 12:13:04 -0700

    Todo Fuera

--- pollo.java
+++ pollo.java
@@ -1,10 +1,13 @@
 public class POLE{
-//YUNG BEEF
-	public String orto;
-	private double peso;
-	private String nombre;
-	public pollo(double peso, String nombre){
-		this.peso = peso;
-		this.noombre = nombre;
-	}
+    //YUNG BEEF
+    public String orto;
+    private double peso;
+    private String nombre;
+    public pollo(double peso, String nombre){
+        this.peso = peso;
+        this.noombre = nombre;
+        rrr
+    }
+    
+    ///YESSSS
 }
