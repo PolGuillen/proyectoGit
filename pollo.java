f3b9d8faf9292f6c7461e240f290476a31975d40
gerard luque <gerardluque@MBP13.local>
2016-03-30 16:45:13 +0200

    tags hehe

--- pollo.java
+++ pollo.java
@@ -1,12 +1,7 @@
 import poleeao;
-public class POLE{
-<<<<<<< HEAD
-//YUNG BEEF
->>>>>>> 9ea657424f9e3e2db9277b9bf4506c6b789d7444
-=======
 //1- YUNG BEEF
->>>>>>> 7bc4a5252e169200dcf0980f7b0b994e943cab83
-	public String orto;
+//tags
+public String orto;
 	private double peso;
 	private String nombre;
 	public pollo(double peso, String nombre){
